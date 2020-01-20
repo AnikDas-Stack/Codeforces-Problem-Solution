0e02479cd950b70b4104761bd52fd1ab44d6f551
Anik Das <60088318+AnikDas-Stack@users.noreply.github.com>
2020-01-20 14:49:38 +0600

    Add files via upload

---
+++ 467A. George and Accommodation.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n, p, q, count=0;
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        scanf("%d %d", &p, &q);
+        if(q-p>=2)
+            count++;
+    }
+    printf("%d\n", count);
+    return 0;
+}
